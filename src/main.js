d1c0347b6b6b519d25752321328d07d97cfd11f6
Selwyn <talk@selwyn.cc>
2017-11-27 21:21:13 +0100

    Before verifying the key check for existence

--- src/main.js
+++ src/main.js
@@ -14,6 +14,13 @@ server
   .launch({ port: process.env.PORT || 3293 });
 
 function verifyKey(key) {
+  if (!key) {
+    throw {
+      message: 'Key not defined',
+      statusCode: 400,
+    };
+  }
+
   return database.get(key).catch(error => {
     if (error.type == 'NotFoundError') {
       return got.head(`https://selwyn.cc/writings/${key}`);
